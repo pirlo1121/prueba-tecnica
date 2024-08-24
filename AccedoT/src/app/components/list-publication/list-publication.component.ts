46bf2cb0aa5cc001bf0145d514237e2cdbd946ee
Maicol bautista <pirlo1121s@gmail.com>
2024-08-23 21:58:26 -0500

    {+} ngx-image-cropper v0.1

--- AccedoT/src/app/components/list-publication/list-publication.component.ts
+++ AccedoT/src/app/components/list-publication/list-publication.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { ImageCroppedEvent, ImageCropperComponent, LoadedImage } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-list-publication',
   standalone: true,
-  imports: [],
+  imports: [ImageCropperComponent],
   templateUrl: './list-publication.component.html',
   styleUrl: './list-publication.component.css'
 })
 export class ListPublicationComponent {
+  imageChangedEvent: Event | null = null;
+  croppedImage: SafeUrl  = '';
 
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+  }
+
+  fileChangeEvent(event: Event): void {
+      this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    if (event.objectUrl) {
+      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
+    }
+    // event.blob can be used to upload the cropped image
+  }
+  imageLoaded(image: LoadedImage) {
+      // show cropper
+  }
+  cropperReady() {
+      // cropper ready
+  }
+  loadImageFailed() {
+      // show message
+  }
 }
